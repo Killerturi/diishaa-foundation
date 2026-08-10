c01743d0de081aef686f41c4f55b61d3e4755a2e
test <killerTuri@gmail.com>
2026-08-10 18:13:03 +0530

    update

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -41,7 +41,7 @@ export default function NewsPage() {
   }, []);
 
   /* =========================================
-     FILTER NEWS
+     FILTER NEWS 
   ========================================== */
 
   const filteredNews = useMemo(() => {
